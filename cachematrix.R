b16c74aaa0457dc8ce1496ec2e2bf82217845bc3
donnatanner10 <84353598+donnatanner10@users.noreply.github.com>
2022-04-13 17:51:30 -0400

    Update cachematrix.R
    completed assignment_draft

--- cachematrix.R
+++ cachematrix.R
@@ -14,13 +14,35 @@ makeCacheMatrix <- function(x = matrix()) {
                     getinverse <- function()x
                     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-## to test makeCacheMAtrix, the test case using m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow=2, ncol=2) was used.m1 returns 
+## to test makeCacheMAtrix, the test case using m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow=2, ncol=2) was used.  m1 returns 
 ##     [,1]  [,2]
 ## [1,]  0.50 -1.00
 ## [2,] -0.25  0.75
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## cacheSolve is a function that will compute the inverse of the matrix object produced by makeCache Matrix. 
+##If the inverse has already been calculated and no changes to the matrix, cacheSolve will retrieve the inverse from the cache.
+  
+  
+  cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    m <- solve(x)
+    if (! is.null(m)){
+      message("getting cached data")
+      return (m)
+    }
+    data <- x$get()
+    m <- inverse(data, ...)
+    x$setinverse(m)
+    m
+  }
+##After defining cacheSolve function, running cacheSolve(m1) one time returns
+## cacheSolve(m1)
+##[,1] [,2]
+##[1,]    6    8
+##[2,]    2    4
+## running cacheSolve(m1) a second time returns
+##cacheSolve(m1)
+##getting cached data
+## [,1] [,2]
+##[1,]    6    8
+## [2,]    2    4
